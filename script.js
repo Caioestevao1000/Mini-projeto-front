8a4161df3a514a01ad74ce96bde96dcf35c83487
Rafael <rafaeldaisu@gmail.com>
2026-06-22 20:00:27 -0300

    /feat - Implement Dialog

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 const searchButton = document.getElementById("search-button");
 const searchInput = document.getElementById("pilot-search");
 const inputError = document.getElementById("input-error");
+const pilotDialog = document.getElementById("pilot-dialog")
+const pilotContent = document.getElementById("pilot-status")
 
+//API search -> returns all pilots once
 async function searchFunction(inputValue){
 
     const response = await fetch("https://api.openf1.org/v1/drivers?&session_key=latest");
@@ -10,20 +13,50 @@ async function searchFunction(inputValue){
 
     const pilot = pilots.find(p => p.full_name.toLowerCase().includes(inputValue.toLowerCase()));
 
-    console.log(pilot);
+    return pilot;
 }
 
-searchButton.addEventListener("click", function(){
+//click event -> validation -> dialog opening
+searchButton.addEventListener("click", async function(){
     const inputValue = searchInput.value;
 
     if (inputValue.length < 3){
         inputError.textContent = "Digite pelo menos 3 caracteres";
         return;
     }
-    
-    inputError.textContent = "Buscando..";
 
-    searchFunction(inputValue);
+    inputError.textContent = "Buscando..";
+    const pilotData = await searchFunction(inputValue);
+
+    if (!pilotData) {
+        inputError.textContent = "Piloto não encontrado";
+        return;
+    }
+
+    console.log(pilotData);
+
+    renderDialog(pilotData);
+
+    openDialog();
 })
+//rendering dialog in HTML
+function renderDialog(pilotData){
+    pilotContent.innerHTML = `
+    <img src="${pilotData.headshot_url}" alt="${pilotData.full_name}"/>
+    <h2>${pilotData.full_name}</h2>
+    <p>${pilotData.team_name}</p>
+    <button onclick="closeDialog()">Fechar</button>
+    `;
+}
+
+// function to open the dialog
+function openDialog(){
+    pilotDialog.showModal();
+}
+
+// function to close the dialog
+function closeDialog(){
+    pilotDialog.close();
+}
 
 
