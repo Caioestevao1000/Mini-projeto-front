278e8b2dc29caeab1f53403400413ac140038fb8
Rafael Okano <rafaeldaisu@gmail.com>
2026-06-23 14:54:32 -0300

    Update script.js

--- script.js
+++ script.js
@@ -58,27 +58,6 @@ function openDialog(){
 function closeDialog(){
     pilotDialog.close();
 }
-// Pesquisa de pilotos
-async function searchFunction(inputValue) {
-  const response = await fetch("https://api.openf1.org/v1/drivers?&session_key=latest");
-  const pilots = await response.json();
-  const pilot = pilots.find(p => p.full_name.toLowerCase().includes(inputValue.toLowerCase()));
-  console.log(pilot);
-}
-
-searchButton.addEventListener("click", function () {
-  const inputValue = searchInput.value;
-
-  if (inputValue.length < 3) {
-    inputError.textContent = "Digite pelo menos 3 caracteres";
-    return;
-  }
-
-  inputError.textContent = "Buscando..";
-  searchFunction(inputValue);
-});
-
-
 // Favoritos
 const btnFavoritos = document.getElementById("btn-favoritos");
 const modalFavoritos = document.getElementById("modal-favoritos");
